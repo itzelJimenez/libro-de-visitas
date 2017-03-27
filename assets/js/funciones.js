cf7192b0199c85f073b4a5e4a1a00aa45c7102d0
itzel <itzeljimenez.do@gmail.com>
2017-03-26 22:44:27 -0600

    Es funcional

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -3,54 +3,63 @@ function color(){
 	//Obtener mediante un prompt el color de fondo que quiera darle a su comentario, guardarlo en una variable.
 	var colorFondo = prompt("Escribe el color de fondo que quieres en Hexadecimal");
 	//Acceder a la caja de texto y asignarle al background el color de fondo dado por el usuario.
-	document.forms[0].elements["caja"].style.background=colorFondo
+	document.getElementById('previo').style.background=colorFondo
 }
 
 function colorLetra(){
 	var colorTexto = prompt("Escribe el color de letra que quieres en Hexadecimal");
-	document.forms[0].elements["caja"].style.color=colorTexto
+	document.getElementById('previo').style.color=colorTexto
 }
 function letraChica(){
-	document.forms[0].elements["caja"].style.fontSize = '10px';
+	document.getElementById('previo').style.fontSize = '10px';
 }
 function letraMediana() {
-	document.forms[0].elements["caja"].style.fontSize = '30px';
+	document.getElementById('previo').style.fontSize = '30px';
 }
 function letraGrande(){
-	document.forms[0].elements["caja"].style.fontSize = '50px';
+	document.getElementById('previo').style.fontSize = '50px';
 }
 function textoIzquierda(){
-	document.forms[0].elements["caja"].style.textAlign = 'left';
+	document.getElementById('previo').style.textAlign = 'left';
 }
 function centrar(){
-	document.forms[0].elements["caja"].style.textAlign = 'center';
+	document.getElementById('previo').style.textAlign = 'center';
 }
 function textoDerecha(){
-	document.forms[0].elements["caja"].style.textAlign = 'right';
+	document.getElementById('previo').style.textAlign = 'right';
 }
 function enviar (){
 	var nodoParrafo = document.createElement("P");
-	var nuevoComentario = document.getElementById("caja").value;
+	var nuevoComentario = document.getElementById("previo").innerHTML;
 	var comentarioTextNode = document.createTextNode(nuevoComentario);
 	var comentarios = document.getElementById('comentarios');
 	nodoParrafo.appendChild(comentarioTextNode);
-	comentarios.appendChild(nodoParrafo);
+	comentarios.insertBefore(nodoParrafo, comentarios.childNodes[0]);
+	
+
 	//IMPORTAR ESTILOS
 	//Importa color de fondo
-	var fondo = document.getElementById("caja").style.background;
+	var fondo = document.getElementById("previo").style.background;
 	nodoParrafo.style.background = fondo;
 	//Importa color de texto
-	var colorLetra = document.getElementById("caja").style.color;
+	var colorLetra = document.getElementById("previo").style.color;
 	nodoParrafo.style.color = colorLetra;
 	//Importa tamaño de texto
-	var size = document.getElementById("caja").style.fontSize;
+	var size = document.getElementById("previo").style.fontSize;
 	nodoParrafo.style.fontSize = size;
 	//Importa alineación de texto
-	var alineacion = document.getElementById("caja").style.textAlign;
+	var alineacion = document.getElementById("previo").style.textAlign;
 	nodoParrafo.style.textAlign = alineacion;
 	
 }
 
+function previsualizar(){
+	var texto = document.getElementById("caja").value
+	var parrafoDePrevisualizacion = document.getElementById('previo');
+	parrafoDePrevisualizacion.innerHTML = texto;
+
+	
+}
 
 
 
